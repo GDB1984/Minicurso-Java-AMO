880990bb287cc7095902d465ed802d61eb16afbf
Guilherme Duarte de Barros <gdbarros1984@gmail.com>
2025-02-04 20:53:01 -0300

    Add files via upload

---
+++ ExemploSaida.java
@@ -0,0 +1,8 @@
+public class ExemploSaida{
+    public static void main (String[] arg){
+        System.out.println ("Olá, meu nome é Guilherme");
+        System.out.println ("Meu curso de graduação é ADS");
+        System.out.println ("O resultado é: " + (23 + 5 <= 22));
+        System.out.println ("Minha idade é 41 anos");
+    }
+}
